474a2e49a45133563ffc7e4d3a6750628ea66928
Lovro Selic <lovro.selic@gmail.com>
2016-12-05 15:02:28 +0100

    Update script.js

--- module4/script.js
+++ module4/script.js
@@ -25,13 +25,12 @@ Good Bye Jim*/
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   for (var x = 0; x< names.length; x++) {
 
-  var firstLetter = names[x].charAt(0).toLowerCase();
-    console.log("firstLetter", firstLetter);
+    var firstLetter = names[x].charAt(0).toLowerCase();
 
-  if (firstLetter === "j") {
-    byeSpeaker.speak();
-  } else {
-    helloSpeaker.speak();
+    if (firstLetter === "j") {
+      byeSpeaker.speak();
+    } else {
+      helloSpeaker.speak();
+    }
   }
-}
 })();
